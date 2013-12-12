f48ee39ee4f609c566441597d19e83e87a61bfb4
linder0209 <linder0209@126.com>
2013-12-12 17:56:56 +0800

    add nodejs content

--- views/nodejs/jshint/examples/jshint_relaxing_options_demo.js
+++ views/nodejs/jshint/examples/jshint_relaxing_options_demo.js
@@ -32,4 +32,59 @@ function test() {
   x += 1; 
 }
 
+//6.  __iterator__
+function iteratorFn (){}
+var iterator = new iteratorFn();
+console.info(iterator.__iterator__);
+
+//7. lastsemic
+var name = (function() { return 'Anton' }());
+
+//8. laxcomma
+var obj = {
+    name: 'Anton'
+  , handle: 'valueof'
+  , role: 'SW Engineer'
+};
+
+//9. loopfunc
+var nums = [];
+for (var i = 0; i < 10; i++) {
+  nums[i] = function (j) {
+    return i + j;
+  };
+}
+nums[0](2); // Prints 12 instead of 2
+
+var nums = [];
+for (var i = 0; i < 10; i++) {
+  (function (i) {
+    nums[i] = function (j) {
+        return i + j;
+    };
+  }(i));
+}   
+
+//10. notypeof
+// 'fuction' instead of 'function'
+var a = '';
+if (typeof a == "fuction") { // Invalid typeof value 'fuction'
+  /* ... */
+}
+
+//11. sub
+var a = {
+  b: '',
+  'c-d': ''
+};
+console.info(a['b']);
+console.info(a['c-d']);
+
+//12. supernew
+var singleton = new function() {
+  var privateVar;
+
+  this.publicMethod  = function () {}
+  this.publicMethod2 = function () {}
+};
 

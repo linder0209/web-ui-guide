d26cab2ac3a5cf1866b2cce1418c5588add7343e
linder0209 <linder0209@126.com>
2014-01-07 18:28:36 +0800

    add nodejs and jsdoc

---
+++ views/JavaScript/examples/jsdoc/jsdoc-demo.js
@@ -0,0 +1,22 @@
+/**
+ * @author linder
+ * @since 2014-1-7
+ * @param {type} arg1
+ * @param {type} arg2
+ * @returns {Number}
+ */
+function jsdocDemo(arg1, arg2) {
+  return arg1 * arg2;
+}
+
+
+
+/**
+ * Represents a book.
+ * @constructor
+ * @param {string} title - The title of the book.
+ * @param {string} author - The author of the book.
+ */
+function Book(title, author) {
+}
+
